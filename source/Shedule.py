3df341641554110aed49ae9e624bb7668814cb7e
apandey13050907 <apandey13050907@gmail.com>
2017-11-19 01:01:31 +0530

    make initial population

--- source/Shedule.py
+++ source/Shedule.py
@@ -24,9 +24,9 @@ def getSlots(s):
 def displayTT(t):	#complete tt
 	for i in range(len(t.table)):
 		print(t.table[i])
-def displayTT(t,g):	#tt for a gievn group
+'''def displayTT(t,g):	#tt for a gievn group
 	for i in range(len(t.table)):
-		print(t.table[i][g-1])
+		print(t.table[i][g-1])'''
 
 class TimeTable:
 	def __init__(self):
@@ -42,34 +42,28 @@ class TimeTable:
 				self.table[pos][int(g)-1].append(i)
 		
 
+population=[]
+def initialise():	#read input data in program
+	global _subject
+	global _group
+	global _class
+	_subject=Subject.read()
+	_group=Group.read()
+	temp=CourseClass.read()
+	for i in range(len(temp)):	#raw class data from .csv file
+		rc=temp[i]	#raw class
+		d=getDuration(rc[0])
+		for j in range(len(rc[1])):	#for each major(lecture) group
+			cc=CourseClass(subject=rc[0],group=rc[1][j],duration=d)
+			for k in range(getSlots(rc[0])):	#for each slot
+				_class.append(cc)
+def init_pop():
+	for i in range(Value.max_pop):
+		population.append(TimeTable())
+		population[i].allot_classes()
+def algorithm():
+	init_pop()
 
-class Shedule:
-	population=[]
-	def initialise():
-		global _subject
-		global _group
-		global _class
-		_subject=Subject.read()
-		_group=Group.read()
-		temp=CourseClass.read()
-		for i in range(len(temp)):	#raw class data from .csv file
-			rc=temp[i]	#raw class
-			d=getDuration(rc[0])
-			for j in range(len(rc[1])):	#for each major(lecture) group
-				cc=CourseClass(subject=rc[0],group=rc[1][j],duration=d)
-				for k in range(getSlots(rc[0])):	#for each slot
-					_class.append(cc)
-
-	def init_pop():
-		for i in range(Value.max_pop):
-			Shedule.population.append(TimeTable())
-			Shedule.population[i].allot_classes()
-	def algorithm():
-		k=1
-
-Shedule.initialise()
-Shedule.init_pop()
-Shedule.algorithm()
-displayTT(Shedule.population[0],1)
-print("---------------")
-displayTT(Shedule.population[15],1)
+initialise()
+algorithm()
+displayTT(population[0])
